e51c30a4b861d54834c57a8d5261e364b53af18b
I303390 <deepshikha.mohanta@sap.com>
2018-05-21 22:52:26 +0530

    Event Binding
    https://www.udemy.com/angular-mastering-the-basics/learn/v4/t/lecture/10121050?start=0

---
+++ binding/src/app/event-binding/event-binding.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-binding',
+  templateUrl: './event-binding.component.html',
+  styleUrls: ['./event-binding.component.css']
+})
+export class EventBindingComponent implements OnInit {
+
+  clickText = "deeps";
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  // Event handler for event binding
+  onClick(){
+    console.log("click me clicked");
+    this.clickText = "Click button clicked and value set with interpolation";
+  }
+
+  onClickPassEvent(e){
+    console.log(e);
+  }
+
+}
