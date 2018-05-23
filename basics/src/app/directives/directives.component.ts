a6d810d5206c8b47894c7707cf22b9914a716733
I303390 <deepshikha.mohanta@sap.com>
2018-05-23 13:46:45 +0530

    ngIf directive
    Elements can be rendered in the DOM based on an if condition.
    This can be done using the ngIf directive.
    
    https://www.udemy.com/angular-mastering-the-basics/learn/v4/t/lecture/10121060?start=0

---
+++ basics/src/app/directives/directives.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directives',
+  templateUrl: './directives.component.html',
+  styleUrls: ['./directives.component.css']
+})
+export class DirectivesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  // STRUCTURAL DIRECTIVES
+  /* Structural Directives help in removing/adding elements to the DOM. 
+  There are 3 main structural Directives :
+   1. ngIf 
+   2. ngSwitch
+   3. ngFor
+    ngIf and ngSwitch directives render html elements based on some condition. 
+    ngFor renders a list of html elements. 
+
+  */
+
+  // A property can also be bound to the ngIf directive
+  public ngIfValue = false;
+  /**
+   * ngIf also has an else block.   
+   */
+
+  public ngIfCondition =false;
+
+}
