85b2c57a6e0cf3bb07ff080e961d4fe822c6f96b
I303390 <deepshikha.mohanta@sap.com>
2018-05-25 23:20:06 +0530

    Wildcard route
    Wildcard route is the route that is added at the end and is loaded if the user clicks on or navigates to a route that does not exist.
    
    Redirection is when coming to one route should internally navigate to another route.
    
    Best way to handle invalid routes is through the wildcard routes.
    With this way, you have a page not found route.
    
    The wild card route should always be the last route of the route config list.
    The flow is that it matches the pattern from top to bottom.
    So make sure that the routes are configured from the most specific to the least specific.
    
    You an also redirect a route to another route.
    This can be done using : redirectTo and pathMatch property of the route.

--- routing/src/app/app-routing.module.ts
+++ routing/src/app/app-routing.module.ts
@@ -2,8 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DepartmentListComponent } from 'src/app/department-list/department-list.component';
 import { EmployeeListComponent } from 'src/app/employee-list/employee-list.component';
+import { PageNotFoundComponent } from 'src/app/page-not-found/page-not-found.component';
 
 const routes: Routes = [
+  {
+    path: "", //default route
+    // component: DepartmentListComponent
+    redirectTo: "/departments",
+    pathMatch:'full' //if '' is a prefix to the route, navigate to /departments
+  },
   {
     path : "departments",
     component: DepartmentListComponent
@@ -11,6 +18,10 @@ const routes: Routes = [
   {
     path : "employees", 
     component: EmployeeListComponent
+  },
+  {
+    path: "**", //this is for all page not found 
+    component: PageNotFoundComponent
   }
 ];
 
